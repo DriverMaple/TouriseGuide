c656c0d97e249eb3d3186f9e6276d8abd7c8c2ca
DriverMaple <763638494@qq.com>
2017-11-02 22:41:08 +0800

    zgw

--- app/src/main/java/com/maple/touriseguide/Fragment/CenFragment.java
+++ app/src/main/java/com/maple/touriseguide/Fragment/CenFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.maple.touriseguide.R;
 
@@ -13,10 +14,24 @@ import com.maple.touriseguide.R;
  */
 
 public class CenFragment extends Fragment{
+    private Button exists;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_cen, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        exists = (Button) view.findViewById(R.id.exist);
+
+        exists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+                System.exit(0);
+            }
+        });
     }
 }
