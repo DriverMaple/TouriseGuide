c8c964db14184b274cf83376e4bc6adef8d6b728
DriverMaple <763638494@qq.com>
2017-11-03 17:19:02 +0800

    zgw

--- app/src/main/java/com/maple/touriseguide/Fragment/TeaFragment.java
+++ app/src/main/java/com/maple/touriseguide/Fragment/TeaFragment.java
@@ -1,10 +1,13 @@
 package com.maple.touriseguide.Fragment;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.maple.touriseguide.R;
 
@@ -12,11 +15,39 @@ import com.maple.touriseguide.R;
  * Created by rrr on 2017/10/26.
  */
 
-public class TeaFragment extends Fragment {
+public class TeaFragment extends Fragment implements View.OnClickListener {
+    private LinearLayout team;
+    private LinearLayout tourise;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_tea, container, false);
+        View view = inflater.inflate(R.layout.fragment_tea, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        team = (LinearLayout) view.findViewById(R.id.team);
+        tourise = (LinearLayout) view.findViewById(R.id.tourise);
+
+        team.setOnClickListener(this);
+        tourise.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.team:
+                tourise.setBackgroundResource(0);
+                team.setBackgroundResource(R.drawable.selector_background_underline);
+                break;
+            case R.id.tourise:
+                team.setBackgroundResource(0);
+                tourise.setBackgroundResource(R.drawable.selector_background_underline);
+                break;
+            default:
+                break;
+        }
     }
 }
